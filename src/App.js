5b7bfeb7128a4976136c3643223fdc810be16bc8
orshah <orshpov@gmail.com>
2023-06-13 00:41:10 -0500

    minor update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import "./App.css";
-import TodoApp from "./components/TodoApp/TodoApp";
 import Avengers from "./components/avengers/Avengers";
 
 function App() {
   return (
     <>
-      {/* <TodoApp /> */}
       <Avengers />
     </>
   );
